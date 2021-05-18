19edde04387d6bce2b753a76c44fb4f04d622b88
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2021-05-18 01:13:15 -0400

    feat!: feature-gate errors

--- src/error.rs
+++ src/error.rs
@@ -7,15 +7,19 @@ pub enum AppResError {
     #[error("cannot find config dir")]
     ConfigDirNotFound,
     /// Could not parse the json when serializing or deserializing.
+    #[cfg(feature = "json_resources")]
     #[error(transparent)]
     InvalidJson(#[from] serde_json::Error),
     /// Could not parse the toml when deserializing.
+    #[cfg(feature = "toml_resources")]
     #[error(transparent)]
     InvalidTomlDeserialization(#[from] toml::de::Error),
     /// Could not parse the toml when serializing.
+    #[cfg(feature = "toml_resources")]
     #[error(transparent)]
     InvalidTomlSerialization(#[from] toml::ser::Error),
     /// Could not parse the yaml when serializing or deserializing.
+    #[cfg(feature = "yaml_resources")]
     #[error(transparent)]
     InvalidYaml(#[from] serde_yaml::Error),
     /// Could not read, write, or access files or directories on the filesystem.
