5d5d09aa6c10bd8a2d96a4ff811b59a1cce0de2a
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2022-10-01 23:55:44 -0700

    doc: add general example and examples for each functions

--- src/resource_types/json.rs
+++ src/resource_types/json.rs
@@ -24,6 +24,28 @@ pub trait JsonResourcesExt {
 
 impl JsonResourcesExt for Resources {
     /// Read json file from resources directory and deserialize it.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```no_run
+    /// use serde::{Deserialize, Serialize};
+    ///
+    /// use appres::Resources;
+    /// // Note you need to enable the json_resources feature in Cargo.toml
+    /// use appres::json::JsonResourcesExt;
+    ///
+    /// #[derive(Deserialize, Serialize)]
+    /// struct Config {
+    ///     stuff: String,
+    /// }
+    ///
+    /// let resources = Resources::new_dir_relative_to_executable("assets").unwrap();
+    ///
+    /// // Load and parse the config.json file in the assets folder
+    /// let config: Config = resources.load_from_json_file("config.json").unwrap();
+    /// ```
     fn load_from_json_file<T>(&self, json_file: impl AsRef<Path>) -> Result<T>
     where
         T: serde::de::DeserializeOwned,
@@ -33,6 +55,29 @@ impl JsonResourcesExt for Resources {
     }
 
     /// Write json file to a path relative from the resources directory.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```no_run
+    /// use serde::{Deserialize, Serialize};
+    ///
+    /// use appres::Resources;
+    /// // Note you need to enable the json_resources feature in Cargo.toml
+    /// use appres::json::JsonResourcesExt;
+    ///
+    /// #[derive(Deserialize, Serialize)]
+    /// struct Config {
+    ///     stuff: String,
+    /// }
+    ///
+    /// let resources = Resources::new_dir_relative_to_executable("assets").unwrap();
+    ///
+    /// // Write config to the config.json file in the assets folder
+    /// let config = Config { stuff: String::from("Hello World") };
+    /// resources.save_to_json_file("config.json", &config).unwrap();
+    /// ```
     fn save_to_json_file<C: ?Sized>(&self, json_file: impl AsRef<Path>, thing: &C) -> Result<()>
     where
         C: serde::Serialize,
@@ -42,6 +87,29 @@ impl JsonResourcesExt for Resources {
     }
 
     /// Write json file to a path relative from the resources directory in a pretty format.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```no_run
+    /// use serde::{Deserialize, Serialize};
+    ///
+    /// use appres::Resources;
+    /// // Note you need to enable the json_resources feature in Cargo.toml
+    /// use appres::json::JsonResourcesExt;
+    ///
+    /// #[derive(Deserialize, Serialize)]
+    /// struct Config {
+    ///     stuff: String,
+    /// }
+    ///
+    /// let resources = Resources::new_dir_relative_to_executable("assets").unwrap();
+    ///
+    /// // Write config to the config.json file in the assets folder (in pretty json format)
+    /// let config = Config { stuff: String::from("Hello World") };
+    /// resources.pretty_save_to_json_file("config.json", &config).unwrap();
+    /// ```
     fn pretty_save_to_json_file<C: ?Sized>(
         &self,
         json_file: impl AsRef<Path>,
@@ -56,6 +124,25 @@ impl JsonResourcesExt for Resources {
 }
 
 /// Deserialize a slice in json format.
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```no_run
+/// use serde::{Deserialize, Serialize};
+///
+/// // Note that you need to enable the json_resources feature in Cargo.toml
+/// use appres::json::load_json_from_slice;
+///
+/// #[derive(Deserialize, Serialize)]
+/// struct Config {
+///     stuff: String,
+/// }
+///
+/// // Parse the bytes as a json object
+/// let config: Config = load_json_from_slice(r#"{"stuff": "Hello World"}"#.as_bytes()).unwrap();
+/// ```
 pub fn load_json_from_slice<T>(json_content: impl AsRef<[u8]>) -> Result<T>
 where
     T: serde::de::DeserializeOwned,
@@ -64,6 +151,25 @@ where
 }
 
 /// Deserialize a string in json format.
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```no_run
+/// use serde::{Deserialize, Serialize};
+///
+/// // Note that you need to enable the json_resources feature in Cargo.toml
+/// use appres::json::load_json_from_str;
+///
+/// #[derive(Deserialize, Serialize)]
+/// struct Config {
+///     stuff: String,
+/// }
+///
+/// // Parse the string as a json object
+/// let config: Config = load_json_from_str(r#"{"stuff": "Hello World"}"#).unwrap();
+/// ```
 pub fn load_json_from_str<T>(json_content: impl AsRef<str>) -> Result<T>
 where
     T: serde::de::DeserializeOwned,
@@ -72,6 +178,26 @@ where
 }
 
 /// Serialize an object into json format and write it to a file as specified by the given path.
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```no_run
+/// use serde::{Deserialize, Serialize};
+///
+/// // Note that you need to enable the json_resources feature in Cargo.toml
+/// use appres::json::save_to_json_file;
+///
+/// #[derive(Deserialize, Serialize)]
+/// struct Config {
+///     stuff: String,
+/// }
+///
+/// // Write the config to config.json
+/// let config = Config { stuff: String::from("Hello World") };
+/// save_to_json_file("config.json", &config).unwrap();
+/// ```
 pub fn save_to_json_file<C: ?Sized>(json_file: impl AsRef<Path>, thing: &C) -> Result<()>
 where
     C: serde::Serialize,
