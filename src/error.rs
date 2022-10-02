bf1f015e32e265943c51de22d63de67ece5167da
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2022-10-01 23:55:39 -0700

    feat: add error for config dir not found

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,8 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum AppResError {
+    #[error("cannot find config dir")]
+    ConfigDirNotFound,
     #[error(transparent)]
     IOError(#[from] std::io::Error),
 }
